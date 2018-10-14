1b77b859c310e0e93d557e4487b872d74e7ef7dc
Phillip Nguyen <phillip-nguyen@outlook.com>
2018-10-14 10:59:03 -0700

    small clean up

--- ToDoAPI/Startup.cs
+++ ToDoAPI/Startup.cs
@@ -36,8 +36,6 @@ namespace ToDoAPI
             Configuration = configuration;
         }
 
-
-
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container.
         /// </summary>
